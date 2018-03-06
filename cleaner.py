a54937a6a67c7fa355a4cfbc605b1307e7ceed02
Juan Pablo Borrero <jpborrero@juans-air.dhcp.nd.edu>
2018-03-05 21:33:27 -0500

    updated cleaning function, reads json.

--- cleaner.py
+++ cleaner.py
@@ -1,12 +1,11 @@
 #Cleaner function
 import pandas as pd
 import numpy as np
-
+import json
+import ast
 
 file = '/Users/jpborrero/Desktop/Data Science/Group Project/Data/movies_metadata.csv'  #CHANGE PATH
 metadata = pd.read_csv(file, dtype={'budget':str, 'revenue':str, 'runtime':str}, low_memory=False)
-
-#print metadata
 
 
 def clean_movie_data(data):
@@ -18,32 +17,27 @@ def clean_movie_data(data):
 			row['budget'] = float(row['budget'])
 		except:
 			row['budget'] = 0
-			#print row['original_title'], row['budget'], 'Fixed budget'
 
 
 		if row['adult'] != False and row['adult'] != True:			#ALL TYPES BOOLEAN
 			row['adult'] = False
-			#print 'Fixed adult'
 
 
 		if type(row['original_language']) != str:			# SOME TYPES ARE FLOAT == NAN
 			row['original_language'] = ''
-		#	print 'Fixed language', row['original_language']
 
 
 		if type(row['popularity']) != float or row['popularity'] == None:    #ALL TYPES FLOAT
 			row['popularity'] = 0
-			#print row['original_title'], row['popularity'], 'Fixed popularity'
 
 
 		if type(row['revenue']) == float:			#SOME TYPES FLOAT == NAN
 			row['revenue'] = 0
 
 		try:
-			row['revenue'] = float(row['budget'])
+			row['revenue'] = float(row['revenue'])
 		except:
 			row['revenue'] = 0
-			#print row['original_title'], row['revenue'], 'Fixed revenue'
 
 
 		if type(row['runtime']) == float:		#SOME TYPES FLOAT == NAN
@@ -56,22 +50,24 @@ def clean_movie_data(data):
 
 
 		if type(row['vote_average']) != float or row['vote_average'] == None:    #ALL TYPES FLOAT
-			row['vote_average] = 0
+			row['vote_average'] = 0
 
+		jsonStr = ast.literal_eval(row['genres'])   #GENRE IS IN JSON FORMAT,
+		json_data = json.dumps(jsonStr)
+		genres = json.loads(json_data)
 
-def merge(data1,data2):
-	finaldf = pd.merge(movies_metadata)
+		try:
+			row['genres'] = genres[0]['name']
+			print genres[0]['name']   #here it changes
+		except:
+			row['genres'] = 'No genre'
 
-1
-2
-3
-4
-result = pd.merge(user_usage,
-                 user_device[['use_id', 'platform', 'device']],
-                 on='use_id')
-result.head()
+	print data
+	return data
 
-clean_movie_data(metadata)
+meta = clean_movie_data(metadata)
+#print meta
+meta.to_csv('cleanedData.csv')
 
 # class Cleaner:
 # 	def clean_movie_data(self, data):
