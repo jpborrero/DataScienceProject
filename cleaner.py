f4c314743e41970403b747199e0d0a4ff704b95d
Juan Pablo Borrero <jpborrero@juans-air.dhcp.nd.edu>
2018-03-25 18:56:19 -0400

    fixes to cleaner function

--- cleaner.py
+++ cleaner.py
@@ -3,73 +3,117 @@ import pandas as pd
 import numpy as np
 import json
 import ast
+import csv
 
 file = 'movies_metadata.csv'  #CHANGE PATH
 metadata = pd.read_csv(file, dtype={'budget':str, 'revenue':str, 'runtime':str}, low_memory=False)
 
 
-def clean_movie_data(data):
-
-	data = data.drop(['belongs_to_collection','homepage', 'imdb_id', 'overview', 'poster_path', 'status', 'tagline', 'video', 'title'], axis=1)
+def count_prod_comp(data):
+ 	data = data.drop(['belongs_to_collection','homepage', 'imdb_id', 'overview', 'poster_path', 'status', 'tagline', 'video', 'title', 'release_date', 'spoken_languages', 'runtime', 'adult'], axis=1)
+	prodCompDict = {}
 	for  index, row in data.iterrows():
-
-		try:										# ALL TYPES ARE STRING
-			row['budget'] = float(row['budget'])
-		except:
-			row['budget'] = 0
-
-
-		if row['adult'] != False and row['adult'] != True:			#ALL TYPES BOOLEAN
-			row['adult'] = False
-
-
-		if type(row['original_language']) != str:			# SOME TYPES ARE FLOAT == NAN
-			row['original_language'] = ''
-
-
-		if type(row['popularity']) != float or row['popularity'] == None:    #ALL TYPES FLOAT
-			row['popularity'] = 0
-
-
-		if type(row['revenue']) == float:			#SOME TYPES FLOAT == NAN
-			row['revenue'] = 0
-
 		try:
-			row['revenue'] = float(row['revenue'])
+			prodCompStr = unicode(row['production_companies'], errors='ignore')   #PRODUCTION COMPANY IS IN JSON FORMAT
+			jsonStr = ast.literal_eval(prodCompStr)
+			json_data = json.dumps(jsonStr)
+			prodComp = json.loads(json_data)
+			if prodComp != []:
+				if prodComp[0]['name'] in prodCompDict:
+					prodCompDict[prodComp[0]['name']] += 1
+				else:
+					prodCompDict[prodComp[0]['name']] = 1
 		except:
-			row['revenue'] = 0
+			continue
 
+	return prodCompDict
 
-		if type(row['runtime']) == float:		#SOME TYPES FLOAT == NAN
-			row['runtime'] = 0
+def clean_movie_data(data, prodCompDict):
+	data = data.drop(['belongs_to_collection','homepage', 'imdb_id', 'overview', 'poster_path', 'status', 'tagline', 'video', 'title', 'release_date', 'spoken_languages', 'runtime', 'adult'], axis=1)
+	with open('cleanedData.csv', 'w') as csvfile:
+		fieldnames = ['budget', 'genres', 'movieId', 'original_language', 'original_title','popularity', 'production_companies', 'production_countries', 'revenue', 'vote_average', 'vote_count']
+		writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+		writer.writeheader()
+		for  index, row in data.iterrows():
+			#BUDGET CLEANING
+			try:										# ALL TYPES ARE STRING
+				row['budget'] = float(row['budget'])
+				if row['budget'] == 0:
+					row['budget'] = -1
+			except:
+				row['budget'] = -1
 
-		try:
-			row['runtime'] = float(row['runtime'])
-		except:
-			row['runtime'] = 0
+			#ORIGINAL LANGUAGE CLEANING
+			if type(row['original_language']) != str:			# SOME TYPES ARE FLOAT == NAN
+				row['original_language'] = ''
 
+			#POPULARITY CLEANING
+			if type(row['popularity']) != float or row['popularity'] == None:    #ALL TYPES FLOAT
+				row['popularity'] = -1
+			if row['popularity'] == 0:
+				row['popularity'] = -1
 
-		if type(row['vote_average']) != float or row['vote_average'] == None:    #ALL TYPES FLOAT
-			row['vote_average'] = 0
+			#GENRE CLEANING
+			jsonStr = ast.literal_eval(row['genres'])   #GENRE IS IN JSON FORMAT
+			json_data = json.dumps(jsonStr)
+			genres = json.loads(json_data)
+			try:
+				row['genres'] = genres[0]['name']
+			except:
+				row['genres'] = ''
 
-		jsonStr = ast.literal_eval(row['genres'])   #GENRE IS IN JSON FORMAT,
-		json_data = json.dumps(jsonStr)
-		genres = json.loads(json_data)
+			#PRODUCTION COMPANY CLEANING
+			try:
+				prodCompStr = unicode(row['production_companies'], errors='ignore')   #PRODUCTION COMPANY IS IN JSON FORMAT
+				jsonStr = ast.literal_eval(prodCompStr)
+				json_data = json.dumps(jsonStr)
+				prodComp = json.loads(json_data)
+				try:
+					if prodCompDict[prodComp[0]['name']] >= 50:
+						row['production_companies'] = prodComp[0]['name']
+					else:
+						row['production_companies'] = 'Other'
+				except:
+					row['production_companies'] = ''
+			except:
+				row['production_companies']=''
 
-		try:
-			row['genres'] = genres[0]['name']
-			#print genres[0]['name']   #here it changes
-		except:
-			row['genres'] = 'No genre'
+			#PRODUCTION COUNTRY CLEANING
+			try:
+				prodCountryStr = unicode(row['production_countries'], errors='ignore')    #PRODUCTION COUNTRY IS IN JSON FORMAT
+				jsonStr = ast.literal_eval(prodCountryStr)
+				json_data = json.dumps(jsonStr)
+				prodCountry = json.loads(json_data)
+				try:
+					row['production_countries'] = prodCountry[0]['name']
+				except:
+					row['production_countries'] = ''
+			except:
+				row['production_countries'] = ''
 
-	#print data
-	return data
+			#REVENUE CLEANING
+			if type(row['revenue']) == float:			#SOME TYPES FLOAT == NAN
+				row['revenue'] = -1
+			try:
+				row['revenue'] = float(row['revenue'])
+			except:
+				row['revenue'] = -1
+			if row['revenue'] == 0:
+				row['revenue'] = -1
 
-meta = clean_movie_data(metadata)
-#print meta
-meta.to_csv('cleanedData.csv')
+			#VOTE AVERAGE CLEANING
+			if type(row['vote_average']) != float or row['vote_average'] == None:    #ALL TYPES FLOAT
+				row['vote_average'] = -1
+			if row['vote_average'] == 0:
+				row['vote_average']=-1
 
-# class Cleaner:
-# 	def clean_movie_data(self, data):
-#  		# return movie data, but without columns we don't need
-#  		return data[['adult', 'budget', 'genres', 'id', 'imdb_id', 'production_companies', 'production_countries', 'revenue', 'runtime', 'spoken_languages', 'title']]
+			#VOTE COUNT CLEANING
+			if type(row['vote_count']) != float or row['vote_count'] == None:    #ALL TYPES FLOAT
+				row['vote_count'] = -1
+			if row['vote_count'] == 0:
+				row['vote_count']=-1
+
+			writer.writerow({'budget':row['budget'], 'genres':row['genres'], 'movieId':row['movieId'], 'original_language':row['original_language'], 'original_title':row['original_title'], 'popularity':row['popularity'], 'production_companies':row['production_companies'], 'production_countries':row['production_countries'], 'revenue':row['revenue'], 'vote_average':row['vote_average'], 'vote_count':row['vote_count']})
+
+prodCompDict = count_prod_comp(metadata)
+clean_movie_data(metadata, prodCompDict)
